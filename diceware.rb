e5596e26d816e40f945a2b6bb93dfe684c08ea10
Zhiming Wang <zmwangx@gmail.com>
2015-06-11 13:32:42 -0700

    new formula: diceware
    https://github.com/zmwangx/diceware

---
+++ diceware.rb
@@ -0,0 +1,17 @@
+class Diceware < Formula
+  homepage "https://github.com/zmwangx/diceware"
+  url "https://github.com/zmwangx/diceware/archive/0.1.tar.gz"
+  sha256 "03ac40c4561039b7d2cf65ccb7648dd8a43be2a8c759c2d575d2377110ef6004"
+
+  head "https://github.com/zmwangx/diceware.git", :branch => "master"
+
+  def install
+    system "make"
+    bin.install "diceware"
+    man1.install "diceware.1"
+  end
+
+  def test
+    assert_equal `#{bin}/diceware`.split().length(), 10
+  end
+end
