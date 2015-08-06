803d8a9e2667b05bbedb475688e9637f68fa8a59
Zhiming Wang <zmwangx@gmail.com>
2015-08-06 14:28:12 -0700

    New formula: tccutil
    Command line tool to add and remove items from the OS X Accessibility
    database.
    
    https://github.com/jacobsalmela/tccutil

---
+++ tccutil.rb
@@ -0,0 +1,17 @@
+class Tccutil < Formula
+  desc "Command line tool to add and remove items from the OS X Accessibility database"
+  homepage "https://github.com/jacobsalmela/tccutil"
+  url "https://github.com/jacobsalmela/tccutil/archive/v1.0.tar.gz"
+  sha256 "cdbe9bd2caf8cc73bd62a1da2e1853ed6634f8e952529eb633e89f906bbec2ce"
+
+  depends_on "coreutils"
+
+  def install
+    system "mkdir", "#{prefix}/bin"
+    system "ginstall", "--target-directory=#{prefix}/bin", "tccutil.py"
+  end
+
+  test do
+    File.exists?("#{bin}/tccutil.py")
+  end
+end
