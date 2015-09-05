3cc45732754a55e4853f6d22b2682d6bb2ccd096
Zhiming Wang <zmwangx@gmail.com>
2015-09-05 10:27:57 +0800

    New cask: ffdec 6.0.1
    JPEXS Free Flash Decompiler.

---
+++ Casks/ffdec.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'ffdec' do
+  version '6.0.1'
+  sha256 '9347b9a0d894cc0d970330c1c07013df452e5bef129bb9e281a3e98c8cb33469'
+
+  # https://github.com/jindrapetrik/jpexs-decompiler
+  url "https://www.free-decompiler.com/flash/download/ffdec_#{version}_macosx.zip"
+  name 'JPEXS Free Flash Decompiler'
+  homepage 'https://www.free-decompiler.com/flash/'
+  license :gpl
+
+  app 'FFDec.app'
+end
