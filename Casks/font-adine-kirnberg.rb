fd95b8213e6f880c78a654402fb6f41b90b07752
Zhiming Wang <zmwangx@gmail.com>
2015-09-21 22:43:00 -0700

    New cask: font-adine-kirnberg

---
+++ Casks/font-adine-kirnberg.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-adine-kirnberg' do
+  version '2.12'
+  sha256 '0d464b34e2dc71d7076b0eb5c0f3e6a242f67d0e6a235ae1c5e8d7c50c03add1'
+
+  url 'https://dl.bintray.com/zmwangx/generic/adine-kirnberg.zip'
+  homepage 'http://www.1001fonts.com/adine-kirnberg-font.html'
+  license :gratis
+
+  font 'adine-kirnberg.alternate.ttf'
+  font 'adine-kirnberg.regular.ttf'
+end
