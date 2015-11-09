eafe5e615c6a462406877ce1dee35410dccf9e87
Zhiming Wang <zmwangx@gmail.com>
2015-11-08 16:38:43 -0800

    trans 0.9.1

--- trans.rb
+++ trans.rb
@@ -2,8 +2,8 @@
 # executable is named trans anyway.
 class Trans < Formula
   homepage "http://www.soimort.org/translate-shell"
-  url "https://github.com/soimort/translate-shell/archive/v0.9.0.9.tar.gz"
-  sha256 "6444f1df693a89760a2ffa5b7c157aed2d7add5fdad759186a21b7943d91c988"
+  url "https://github.com/soimort/translate-shell/archive/v0.9.1.tar.gz"
+  sha256 "dc4986e879d4b8c2c974e912b82e2e1db25f229a2b75b59bb2967e80c0d0dd85"
 
   head "https://github.com/soimort/translate-shell.git", :branch => "develop"
 
