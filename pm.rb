304d043198cc3f35045718797cd16e458e6af388
Zhiming Wang <zmwangx@gmail.com>
2016-07-02 11:38:43 +0800

    pm 0.2

--- pm.rb
+++ pm.rb
@@ -1,8 +1,8 @@
 class Pm < Formula
   desc "Preview man page in your browser as you edit"
   homepage "https://github.com/zmwangx/pm"
-  url "https://github.com/zmwangx/pm/archive/v0.1.1.tar.gz"
-  sha256 "4fcdbae4f4821e4d15bef2d64e9c07212d47308fa226a9f571fb41dbfb7fd23d"
+  url "https://github.com/zmwangx/pm/archive/v0.2.tar.gz"
+  sha256 "48f29311e01ab54076f09334632af8b5abb4bcc33d40ed3b6739a3b3038ea10e"
   head "https://github.com/zmwangx/pm.git"
 
   depends_on "autoconf" => :build
